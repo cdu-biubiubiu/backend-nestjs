209eb0247bd674a6a627e819e081b771f2d06d89
Han Han <haannn@qq.com>
2020-08-30 10:57:14 +0800

    build: init service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { LinkController } from './controller/link/link.controller';
 import { UserController } from './controller/user/user.controller';
 import { PostController } from './controller/post/post.controller';
 import { TestController } from './controller/test/test.controller';
+import { UserService } from './service/user/user.service';
 
 @Module({
   imports: [],
   controllers: [AppController, LinkController, UserController, PostController, TestController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
