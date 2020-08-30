fcf85274035d33d21079ecedd1dccc06049bc088
Han Han <haannn@qq.com>
2020-08-30 16:49:20 +0800

    add validator support

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsAlphanumeric, IsEnum, IsString } from "class-validator";
 
 export enum Score {
   Admin = "admin",
@@ -6,16 +7,20 @@ export enum Score {
   User = "user",
 }
 export class CreateUserDto {
+  @IsString()
   @ApiProperty({
     minimum: 6,
     maximum: 16,
   })
   username: string;
+  @IsString()
+  @IsAlphanumeric()
   @ApiProperty({
     minimum: 6,
     maximum: 16,
   })
   password: string;
+  @IsEnum(Score)
   @ApiProperty({
     enum: Score,
   })
