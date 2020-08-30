39cedd71dc1c1e37f97d84f708e5429ef030f0f6
Han Han <haannn@qq.com>
2020-08-30 16:19:55 +0800

    add jwt support

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { UserService } from "../user/user.service";
+import { JwtService } from "@nestjs/jwt";
+import { User } from "../interface/user.interface";
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private jwtService: JwtService) {}
   async validateUser(username: string, password: string) {
     const user = await this.userService.findOneByUsername(username);
     if (user && user.password === password) {
@@ -11,5 +13,11 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+  async login(user: User) {
+    const payload = { username: user.username };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
