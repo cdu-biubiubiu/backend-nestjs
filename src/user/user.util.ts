653d7f51da37dbcdbb74affecea6a12279dffd0f
Han Han <haannn@qq.com>
2020-09-03 10:46:34 +0800

    修改工具类

--- src/user/user.util.ts
+++ src/user/user.util.ts
@@ -1,6 +1,10 @@
 import { User } from "./user.schema";
+import { CreateUserDto } from "./dto/create-user.dto";
 
-export function filterPassword(user: User) {
-  const { password, ...result } = user.toObject();
+export function filterPassword(user: User | CreateUserDto) {
+  if (user instanceof User) {
+    user = user.toObject();
+  }
+  const { password, ...result } = user;
   return result;
 }
